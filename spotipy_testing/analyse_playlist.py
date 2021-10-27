bdeb88584fc805c20aacb3f82dd17b2d2eca193f
Shaylan Rao <sxr980@student.bham.ac.uk>
2021-10-27 17:34:25 +0100

    added third playlist (emotion based), more structured code

--- spotipy_testing/analyse_playlist.py
+++ spotipy_testing/analyse_playlist.py
@@ -10,67 +10,61 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='ed61ad2eb1ac48f38a5971
                                                client_secret='3977cda8a7a14e63b5bdf985c0a5b440', scope=scope,
                                                redirect_uri='http://localhost:8080', show_dialog=True))
 
-vary = 'energy'
-varx = 'valence'
-
 
 def get_attribute(featuresp, attribute):
-    return [featuresp[i][attribute] for i in range(len(features))]
+    return [featuresp[i][attribute] for i in range(len(featuresp))]
+
+# Returns attributes x, y, z given playlist id
+def get_x_y_z(pl_id):
+    offset = 0
+
+    #  Get a list of all tracks on playlist
+    response = sp.playlist_items(pl_id,
+                                 offset=offset,
+                                 fields='items.track.id,total',
+                                 additional_types=['track'])
+
+    song_list_ids = []
+    for song in (response['items']):
+        song_list_ids.append(song['track']['id'])
+
+    # Get features for each song in list
+    features = sp.audio_features(song_list_ids)
+
+    print(features[0])
+
+    y = get_attribute(features, vary)
+    x = get_attribute(features, varx)
+    z = get_attribute(features, varz)
+
+    return x, y, z
 
 
-# Get playlist 'Happy playlist' via id
-pl_id = '0IAG5sPikOCo5nvyKJjCYo'
+def show_graph():
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
 
-offset = 0
-
-#  Get a list of all tracks on playlist
-response = sp.playlist_items(pl_id,
-                             offset=offset,
-                             fields='items.track.id,total',
-                             additional_types=['track'])
-
-song_list_ids = []
-for song in (response['items']):
-    song_list_ids.append(song['track']['id'])
-
-# Get features for each song in list
-features = sp.audio_features(song_list_ids)
-
-y1 = get_attribute(features, vary)
-x1 = get_attribute(features, varx)
+    ax.scatter(x1, y1, z1, c='y', marker='o')
+    ax.scatter(x2, y2, z2, c='b', marker='o')
+    ax.scatter(x3, y3, z3, c='r', marker='o')
 
 
-#########################################################################
+    ax.set_xlabel(varx)
+    ax.set_ylabel(vary)
+    ax.set_zlabel(varz)
 
-pl_id = '3rsdTIkZaDaZT5gL5oPPo1'
+    plt.show()
 
-offset = 0
 
-#  Get a list of all tracks on playlist
-response = sp.playlist_items(pl_id,
-                             offset=offset,
-                             fields='items.track.id,total',
-                             additional_types=['track'])
+# Define the 3 variables
+varx = 'valence'
+vary = 'energy'
+varz = 'speechiness'
 
-song_list_ids = []
-for song in (response['items']):
-    song_list_ids.append(song['track']['id'])
+x1, y1, z1 = get_x_y_z('0IAG5sPikOCo5nvyKJjCYo')
+x2, y2, z2 = get_x_y_z('78FHjijA1gBLuVx4qmcHq6')
+x3, y3, z3 = get_x_y_z('3aBeWOxyVcFupF8sKMm2k7')
 
-# Get features for each song in list
-features = sp.audio_features(song_list_ids)
 
-y2 = get_attribute(features, vary)
-x2 = get_attribute(features, varx)
+show_graph()
 
-plt.plot(x1, y1, 'bo')
-plt.plot(x2, y2, 'bo')
-plt.xlabel(varx)
-plt.ylabel(vary)
-plt.plot(range(1))
-plt.yticks(np.arange(0, 1, 0.1))
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-
-plt.show()
-
-print(features[2])
